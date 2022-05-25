bf23627477550f47cd48d09bf9d34e3e13b13114
ThanhNguyenDat <johnkelvin3107@gmail.com>
2022-05-25 20:47:17 +0700

    upload

--- utils.py
+++ utils.py
@@ -57,11 +57,50 @@ def Rz(theta):
                      [0, 0, 1]])
 
 
-def create_point_cloud_rotation(pcd, rotation_matrix) -> o3d.geometry.PointCloud():
-    n_xyz = np.asarray(pcd.points)
-    n_xyz_rotated = np.dot(n_xyz, rotation_matrix)
-    pcd_rotated = create_pcd(n_xyz_rotated)
-    return pcd_rotated
+# vector_init_x = (1, 0, 0)
+# vector_init_y = (0, 1, 0)
+# vector_init_z = (0, 0, 1)
+# vector_object = (a, b, c)
+
+def rotation_matrix_3x3_from_vectors(vec1, vec2):
+    """ Find the rotation matrix that aligns vec1 to vec2
+    :param vec1: A 3d "source" vector
+    :param vec2: A 3d "destination" vector
+    :return mat: A transform matrix (3x3) that maps vec1 to vec2
+    """
+    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
+    v = np.cross(a, b)
+
+    if any(v): #if not all zeros then 
+        c = np.dot(a, b)
+        s = np.linalg.norm(v)
+        
+        kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+        return np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+
+    else:
+        return np.eye(3)
+
+def transform_matrix_from_vector(vec, axis=(0, 0, 1)):
+    """ Find the transform matrix that aligns vec1 to vec2
+    :param vec: A 3d "source" vector
+    :return mat: A transform matrix (4x4) that maps vec1 to vec2
+    """
+    mat = np.eye(4)
+    mat[:3,:3] = rotation_matrix_3x3_from_vectors(vec, axis)
+    return mat
+    
+
+# mat = rotation_matrix_3x3_from_vectors(vector_object, vector_init_z)
+# print(mat)
+# # vec_rot = mat.dot(vector_object)
+
+# vec_trans = transform_matrix_from_vector(vector_object, (0, 0, 1))
+# print(vec_trans)
+
+# # pcd after rotation
+# print("pcd before rotation: ", np.asarray(pcd.points))
+# print("pcd after rotation: ", np.asarray(pcd.transform(vec_trans)))
 
 
 def plane_seg(pcd, 
