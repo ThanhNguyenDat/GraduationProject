b7a1fbe66d633acde17c3eae774ddc9d0fb75996
ThanhNguyenDat <johnkelvin3107@gmail.com>
2022-06-03 17:02:41 +0700

    upload translate

--- Thanh/test_4.py
+++ Thanh/test_4.py
@@ -1,6 +1,22 @@
 # from utils import *
 import numpy as np
 
+def Rx(theta):
+  return np.matrix([[ 1, 0           , 0           ],
+                   [ 0, np.cos(theta),-np.sin(theta)],
+                   [ 0, np.sin(theta), np.cos(theta)]])
+  
+def Ry(theta):
+  return np.matrix([[ np.cos(theta), 0, np.sin(theta)],
+                   [ 0           , 1, 0           ],
+                   [-np.sin(theta), 0, np.cos(theta)]])
+  
+def Rz(theta):
+  return np.matrix([[ np.cos(theta), -np.sin(theta), 0 ],
+                   [ np.sin(theta), np.cos(theta) , 0 ],
+                   [ 0           , 0            , 1 ]])
+
+
 def get_vector_init(vector_object, vector_target):
     """
     Get the vector init.
@@ -10,14 +26,19 @@ def get_vector_init(vector_object, vector_target):
 coor_O = np.array([0, 0, 0]) # coordinate base on O
 coor_c_for_O = np.array([10, 10, 3]) # coordinate camera for base 0,0,0
 coor_p_for_C = np.array([0, 0, 3]) # coordinate P points for camera
-coor_p_for_C_2 = np.array([5, 6, 4])
+coor_p_for_C_2 = np.array([5, 6, 4]) # coordinate P_2 points for camera
 
 def get_coor_p_for_O(coor_0, coor_c_for_O, coor_p_for_C):
     """
     Get the coordinate P points for base 0,0,0.
     """
-    return coor_p_for_C - coor_c_for_O - coor_0
+    return coor_p_for_C + coor_c_for_O + coor_0
 
 coor_P = get_coor_p_for_O(coor_O, coor_c_for_O, coor_p_for_C)    
-print(coor_P)
+print("Toa do diem P so voi diem O", coor_P)
+# [10, 10, 6]
 
+
+coor_P_2 = get_coor_p_for_O(coor_O, coor_c_for_O, coor_p_for_C_2)
+print("Toa do diem P2 so voi diem O", coor_P_2)
+# [15, 16, 7]
